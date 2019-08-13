ae336534c485dc372260ae608b9bc7a5953ce1e9
Harjot Singh Malhi <hsmalhi94@hotmail.com>
2019-08-13 18:40:51 +0000

    Add tweet container and associated css

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
   
   //Event handler which manages the changing of the character counter
-  $("#new-tweet-text").on("keyup", function() {
+  $("textarea[name=text]").on("keyup", function() {
     let tweetLength = $(this).val().length;
-    $(this).siblings(".counter")[0].innerHTML = 140 - tweetLength;
+    let counter = $(this).siblings(".counter")[0];
+    counter.innerHTML = 140 - tweetLength;
+    if(counter.innerHTML >= 0) {
+      counter.className = "counter valid";
+    } else {
+      counter.className = "counter invalid";
+    }
   });
 });
